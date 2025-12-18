d85b4c28bdd7de78034dd8ec6405d9d851b8b24c
Nils Furler <e228419@edu.sbl.ch>
2025-12-18 13:57:20 +0100

    Layout verbessert und Unterseite geändert

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-let counter = 0
+/* Erstellt bei jedem Funktionsaufruf einen neuen nummerierten Absatz und fügt ihn in main ein */
+let counter = 0;
 
 function createNewElement() {
-let newElement = document.createElement("p")
+  let newElement = document.createElement("p");
 
-counter = counter + 1
+  counter = counter + 1;
 
-newElement.textContent = "Element" + counter
+  newElement.textContent = "Element" + counter;
 
-let mainElement = document.querySelector("main");
-mainElement.appendChild(newElement)
+  let mainElement = document.querySelector("main");
+  mainElement.appendChild(newElement);
 }
